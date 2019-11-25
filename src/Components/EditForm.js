d7fca2bb279237f41c43da3d4cdb67dcd5a9f176
Kenneth <sally@sallyde-MacBook-Air.local>
2019-11-24 21:10:57 -0800

    fix minor bugs

--- src/Components/EditForm.js
+++ src/Components/EditForm.js
@@ -1,7 +1,6 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import useInputState from "../Hooks/useInputState"
-import useToggle from "../Hooks/useToggle";
 
 export default function EditForm({editTodo,id, task,toggle}) {
   const [value, handleChange, reset] = useInputState(task);
